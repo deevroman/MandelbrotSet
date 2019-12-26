6c3171f83a16a58654282465f88fb415a2acb7f0
deevroman <roman.deev06@gmail.com>
2019-12-26 12:22:01 +0300

    Final

--- cmake-build-debug/CMakeFiles/Fractal_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Fractal_autogen"
-  "Fractal_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Fractal_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
